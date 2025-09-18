3d90f0bf191a6ebc922107b9b60cf86092c75057
Bambam Kumar Gupta <bambamkumar30082003.ara@gmail.com>
2025-09-18 07:45:19 +0530

    Is Subsequence.cpp

---
+++ Is Subsequence.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int n;
+    int m;
+    int LCS(string s, string t){
+
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+
+        for(int i=0; i<=n; i++){
+            dp[i][0] = 0;
+        }
+
+        for(int j=0; j<=n; j++){
+            dp[0][j] = 0;
+        }
+
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(s[i-1]==t[j-1]){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n][m];
+    }
+    bool isSubsequence(string s, string t) {
+
+        n = s.size();
+        m = t.size();
+
+        if(t.size()<s.size()) return false;
+
+        int len = LCS(s, t);
+
+        if(len==n){
+            return true;
+        }
+        return false;
+    }
+};
