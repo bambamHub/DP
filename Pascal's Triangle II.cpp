bd432a318f2b8e4457a79d6df89656d7c0f71550
Bambam Kumar Gupta <bambamkumar30082003.ara@gmail.com>
2025-09-18 07:45:59 +0530

    Pascal's Triangle II.cpp

---
+++ Pascal's Triangle II.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> getRow(int rowIndex){
+
+        vector<vector<int>> res(rowIndex+1);
+
+        for(int i=0; i<=rowIndex; i++){
+
+            res[i] = vector<int>(i+1, 1);
+            for(int j=1; j<i; j++){
+                res[i][j] = res[i-1][j] + res[i-1][j-1];
+            }
+        }
+
+        return res[rowIndex];
+        
+    }
+};
